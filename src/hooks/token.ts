7c3d2aac58fa2df6e11616232ad7dc2dbdd017dd
David Mihal <david@dmihal.com>
2021-07-11 22:15:11 +0100

    Don't approve if allowance

--- src/hooks/token.ts
+++ src/hooks/token.ts
@@ -6,6 +6,7 @@ import { ethers } from 'ethers'
 const ERC20_ABI = [
   'function balanceOf(address owner) external view returns (uint256)',
   'function approve(address spender, uint256 amount) external',
+  'function allowance(address owner, address spender) external view returns (uint256)',
 ]
 
 function useTokenContract(address: string): Contract | null {
@@ -14,9 +15,15 @@ function useTokenContract(address: string): Contract | null {
 
 export function useApprove(address: string, spender: string, amount: any) {
   const contract = useTokenContract(address)
+  const { account } = useActiveWeb3React()
 
   return async () => {
     if (contract) {
+      const allowance = await contract.allowance(account, spender)
+      if (allowance.gte(amount)) {
+        return
+      }
+
       const tx = await contract.approve(spender, amount)
       await tx.wait()
     }
